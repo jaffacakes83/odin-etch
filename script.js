3626f58315e6962ec405924ad99e8dda6214d6b5
jaffacakes83 <124577027+jaffacakes83@users.noreply.github.com>
2023-09-12 22:28:09 +0100

    Added JS to create grid element

---
+++ script.js
@@ -0,0 +1,6 @@
+const elGameGrid = document.getElementById('game-container');
+
+const gameSquare = document.createElement('div');
+gameSquare.classList.add('game-square');
+
+elGameGrid.appendChild(gameSquare);
